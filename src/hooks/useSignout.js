c43db8d0f79f501d936b7edb9eb6147462014db8
HashirAKB <ahmedhashir96@gmail.com>
2024-07-16 14:04:36 +0530

    Implement sign-out functionality with state reset.

---
+++ src/hooks/useSignout.js
@@ -0,0 +1,25 @@
+// src/hooks/useSignout.js
+import { useSetRecoilState } from "recoil"
+import { ConfirmPasswordState, IsEditingState, jwtTokenState, passwordState, usernameState } from "../store/atoms/userAtoms"
+import { useNavigate } from "react-router-dom";
+import { todoState } from "../store/atoms/todoAtoms";
+
+export const useSignout = () => {
+    const clearJWT = useSetRecoilState(jwtTokenState);
+    const clearTodos = useSetRecoilState(todoState);
+    const clearUser = useSetRecoilState(usernameState);
+    const clearPassword = useSetRecoilState(passwordState);
+    const clearConfirmPassword = useSetRecoilState(ConfirmPasswordState);
+    const clearIfIsEdit = useSetRecoilState(IsEditingState);
+    const navigate = useNavigate();
+
+    return () => {
+        clearJWT('');
+        clearTodos([]);
+        clearUser('');
+        clearPassword('');
+        clearConfirmPassword('');
+        clearIfIsEdit(false);
+        navigate('/');
+    };
+}
