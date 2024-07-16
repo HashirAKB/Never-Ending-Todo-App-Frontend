54987f671ee03e76d3c4ecb4a075fd55f60d8cfb
HashirAKB <ahmedhashir96@gmail.com>
2024-07-16 14:02:02 +0530

    feat: enhance NavBar with dynamic navigation and sign-out logic
    Refactored NavBar component to use custom hook for sign-out functionality and adjusted navigation links based on user authentication status.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,27 @@
 import { Link } from "react-router-dom"
 import { useRecoilValue } from "recoil"
 import { jwtTokenState } from "../store/atoms/userAtoms"
-import { SignoutButton } from "./SignoutButton"
+import { useSignout } from "../hooks/useSignout"
 export const NavBar = () => {
+
     const jwtToken = useRecoilValue(jwtTokenState);
+    const signout = useSignout();
+
     return(
         <>
             <Link to="/">
                 <h2>ToDo</h2>
-            </Link>                     
-            <Link to={"/todos"}>
-                <button>Todos</button>
-            </Link>
-            <Link to={"/profile"}>
-                <button>View profile</button>
-            </Link> 
+            </Link>                      
             {jwtToken ?
-                    <SignoutButton/>
+                <>
+                    <Link to={"/todos"}>
+                        <button>Todos</button>
+                    </Link>
+                    <Link to={"/profile"}>
+                        <button>View profile</button>
+                    </Link>
+                    <button onClick={() => {signout}}>SignOut</button>
+                </>
                     :
                     <>
                         <Link to={"/signup"}>
